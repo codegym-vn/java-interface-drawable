96005dff5d15b2a130b9ffe453a88fde0e679ebc
Nguyen Khac Nhat <khacnhat.info@gmail.com>
2019-05-08 16:16:43 +0700

    Refactor Picture

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    Picture picture = new Picture();
+        picture.add(new Rectangle(10, 5));
+        picture.add(new House());
+        picture.add(new Rectangle(20, 5));
 	    picture.show();
     }
 }
--- src/com/codegym/Picture.java
+++ src/com/codegym/Picture.java
@@ -2,18 +2,23 @@ package com.codegym;
 
 public class Picture {
 
-    private Drawable[] elemenets;
+    private Drawable[] elements;
+    public static final int MAX_COUNT = 100;
+    private int count = 0;
 
     public Picture() {
-        this.elemenets = new Drawable[3];
-        this.elemenets[0] = new Rectangle(10, 5);
-        this.elemenets[1] = new House();
-        this.elemenets[2] = new Rectangle(20, 5);
+        this.elements = new Drawable[MAX_COUNT];
+    }
+
+    public void add(Drawable element) {
+        if(count < MAX_COUNT) {
+            this.elements[count] = element;
+        }
     }
 
     public void show() {
-        for (int i = 0; i < this.elemenets.length; i++) {
-            this.elemenets[i].draw();
+        for (int i = 0; i < this.count; i++) {
+            this.elements[i].draw();
             System.out.println();
         }
     }
